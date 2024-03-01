caef67d5f51a642d6044649b74fa3167f27a2f73
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-01 20:22:23 +0530

    feat: student service

---
+++ src/app/student/student.model.ts
@@ -0,0 +1,5 @@
+export interface StudentModel {
+  id: string;
+  name: string;
+  email: string;
+}
---
+++ src/app/student/student.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject } from "@angular/core";
+import { environment } from "../../environments/environment.development";
+import { Observable } from "rxjs";
+import { StudentModel } from "./student.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: "root" })
+export class StudentService {
+  private apiUrl = environment.API_BASE_URL;
+  private http = inject(HttpClient);
+
+  addStudent(student: StudentModel): Observable<StudentModel> {
+    return this.http.post<StudentModel>(this.apiUrl, student);
+  }
+
+  getStudents(): Observable<StudentModel[]> {
+    return this.http.get<StudentModel[]>(this.apiUrl);
+  }
+
+  getStudent(id: string): Observable<StudentModel> {
+    return this.http.get<StudentModel>(`${this.apiUrl}/${id}`);
+  }
+
+  deleteStudent(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+
+  updateStudent(student: StudentModel): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${student.id}`, student);
+  }
+}
