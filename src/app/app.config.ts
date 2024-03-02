2847a3854f9143354c3e766105c2b89eee85df95
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-02 11:28:14 +0530

    feat: book reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,23 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideStore } from "@ngrx/store";
 import { provideHttpClient } from "@angular/common/http";
-import { provideEffects } from '@ngrx/effects';
+import { provideEffects } from "@ngrx/effects";
+import {
+  StudentFeatureKey,
+  studentReducer,
+} from "./student/store/book.reducer";
+
+const reducers = {
+  [StudentFeatureKey]: studentReducer,
+};
+
+// const effects = [];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideHttpClient(), provideEffects()],
+  providers: [
+    provideRouter(routes),
+    provideStore(reducers),
+    provideHttpClient(),
+    // provideEffects(effects),
+  ],
 };
--- src/app/student/store/book.reducer.ts
+++ src/app/student/store/book.reducer.ts
@@ -1,1 +1,76 @@
-export const BookFeatureKey = "student";
+import { HttpErrorResponse } from "@angular/common/http";
+import { StudentModel } from "../student.model";
+import { createReducer, on } from "@ngrx/store";
+import { StudentActions } from "./student.actions";
+
+export const StudentFeatureKey = "student";
+
+export interface StudentState {
+  students: StudentModel[];
+  loading: boolean;
+  error: HttpErrorResponse | null;
+}
+
+export const initialState: StudentState = {
+  students: [],
+  loading: false,
+  error: null,
+};
+
+export const studentReducer = createReducer(
+  initialState,
+  on(StudentActions.loadStudents, (state) => ({ ...state, loading: true })),
+  on(StudentActions.loadStudentSuccess, (state, { students }) => ({
+    ...state,
+    students,
+    loading: false,
+  })),
+  on(StudentActions.loadStudentFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  })),
+  on(StudentActions.addStudent, (state, { student }) => ({
+    ...state,
+    loading: true,
+  })),
+  on(StudentActions.addStudentSuccess, (state, { student }) => ({
+    ...state,
+    students: [...state.students, student],
+    loading: false,
+  })),
+  on(StudentActions.addStudentFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  })),
+  on(StudentActions.updateStudent, (state, { student }) => ({
+    ...state,
+    loading: true,
+  })),
+  on(StudentActions.updateStudentSuccess, (state, { student }) => ({
+    ...state,
+    loading: false,
+    students: state.students.map((s) => (s.id === student.id ? student : s)),
+  })),
+  on(StudentActions.updateStudentFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  })),
+
+  on(StudentActions.deleteSdudent, (state, { id }) => ({
+    ...state,
+    loading: true,
+  })),
+  on(StudentActions.deleteSdudentSuccess, (state, { id }) => ({
+    ...state,
+    loading: false,
+    students: state.students.filter((s) => s.id !== id),
+  })),
+  on(StudentActions.deleteStudentFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  }))
+);
